2e3d533a079c3783fcaf0979bbe4aea0d1d69289
Shatlykch <157189400+Shatlykch@users.noreply.github.com>
2024-04-02 16:08:56 -0400

    completed

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,15 @@
 # import Home view from the views file
+from .views import Home, MovieList, MovieDetail  # additional imports
 from .views import Home
 from django.urls import path
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+]
+
+urlpatterns = [
+    path('', Home.as_view(), name='home'),
+    # new routes below
+    path('movies/', MovieList.as_view(), name='cat-list'),
+    path('movies/<int:id>/', MovieDetail.as_view(), name='movie-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,24 @@
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from .serializers import MovieSerializer
 # Define the home view
+from rest_framework import generics
+from .models import Movie
 
 
 class Home(APIView):
     def get(self, request):
         content = {'message': 'Welcome to the movies-collector api home route!'}
         return Response(content)
+
+
+class MovieList(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    lookup_field = 'id'
